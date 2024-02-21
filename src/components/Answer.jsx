1f1aed25a5124a513a5f580f6181b007ff93cf9e
elhuzain <a.huzain98@gmail.com>
2024-02-21 17:35:22 +0200

    fixed focus

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,6 +1,8 @@
+import { useRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const Answer = ({ id, checked, value, SelectOption }) => {
+  const inputRef = useRef();
 
   // Delay to create staggering animation.
   const delay =
@@ -10,9 +12,9 @@ const Answer = ({ id, checked, value, SelectOption }) => {
           : "300ms"
 
   return (
-    <label style={{ animationDelay: delay }} key={value} tabIndex={0} htmlFor={id} className={twMerge("p-4 answer-slide-in group w-full border-2 hover:translate-y-[-3px] cursor-pointer transition-transform relative border-foreground flex items-center rounded-md", checked && "bg-foreground text-background")}>
+    <label style={{ animationDelay: delay }} key={value} htmlFor={id} className={twMerge("p-4 answer-slide-in focusable-label group w-full border-2 hover:translate-y-[-3px] cursor-pointer transition-transform relative border-foreground flex items-center rounded-md", checked && "bg-foreground text-background")}>
 
-      <input tabIndex={-1} checked={checked} type="checkbox" onChange={() => { SelectOption(id) }} id={id} className="opacity-0 z-0 w-[0px] h-[0px] absolute" />
+      <input ref={inputRef} checked={checked} type="checkbox" onChange={() => { SelectOption(id) }} id={id} className="opacity-0 peer z-0 w-[0px] h-[0px] absolute" />
       <h2 className="pointer-events-none">
 
         <span className="mr-4">

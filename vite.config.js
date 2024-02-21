19a5c6f9abf8ed7facdde000301b0defc455afce
elhuzain <a.huzain98@gmail.com>
2024-02-21 04:39:22 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://elhuzain.github.io/quiz-app/",
+  build: {
+    outDir: "build"
+  }
 })
